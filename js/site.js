d88cbae08d872613ed7a70b10e7774edf4fe84dd
CarlosMInfante <83223613+CarlosMInfante@users.noreply.github.com>
2021-10-26 18:56:43 -0400

    Creating first function in site file

--- js/site.js
+++ js/site.js
@@ -1,4 +1,25 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//Get the values from the interface
+//Start or controller function
+function getValues(params) {
+    //get values from the page
+    // "let" is a block scoped keyword here because inside the function.
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    alert("The Start Value:" + startValue);
+    //we call generateNumbers
+
+    //we call displayNumbers
+}
+
+//Generate numbers from the startValue to the endValue
+//Logic function(s)
+function generateNumbers(params) {
+    
+}
+
+//Displace the numbers and mark the even numbers bold.
+//Display or view functions.
+function displayNumbers(params) {
+    
 }
